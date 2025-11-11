cb71f86005d9cfd8111c47e1ef544205c8e53942
ravindra <ravindraprasadpoodota@gmail.com>
2025-11-11 23:42:05 +0530

    initial Commit

---
+++ loan-application-service/src/main/java/com/knights/loanapplication/LoanApplicationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.knights.loanapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApplicationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanApplicationServiceApplication.class, args);
+	}
+
+}
