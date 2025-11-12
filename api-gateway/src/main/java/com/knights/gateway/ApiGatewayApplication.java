eb123f4700253e5149de60fa86420032c0171462
user <user@DESKTOP-A941V1O>
2025-11-12 16:08:08 +0530

    For Config
    Signed-off-by: user <user@DESKTOP-A941V1O>

--- api-gateway/src/main/java/com/knights/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/knights/gateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.knights.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // only if you use Eureka
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
